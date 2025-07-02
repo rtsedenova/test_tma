af8a35982e5a235a8bc020181e091e6d105d3f37
renata tsedenova <recedenova@gmail.com>
2025-07-02 05:16:51 +0300

    removed old useless file

--- server/src/bot/handlers/createpost.command.ts
+++ server/src/bot/handlers/createpost.command.ts
@@ -3,11 +3,15 @@ import { setState } from '../state/createpost.state';
 
 export async function handleCreatePostCommand(ctx: Context) {
   console.log('Received /createpost command');
-  if (ctx.from?.username !== 'renatayeoo') {
-    console.log('Unauthorized user tried to use /createpost');
+
+  const allowedUsernames = ['renatayeoo', 'PTRLS'];
+
+  if (!allowedUsernames.includes(ctx.from?.username || '')) {
+    console.log('Unauthorized user tried to use /createpost:', ctx.from?.username);
     await ctx.reply('Unknown command');
     return;
   }
+
   setState(ctx.chat!.id, { step: 'awaiting_title' });
   await ctx.reply('Send the title first, then description and then the photo');
-} 
+}
